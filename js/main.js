26c2d89db7ee1028a2a0bdd79b19a066575ab7a5
ner-develop <144132227+ner-develop@users.noreply.github.com>
2024-01-04 02:44:49 +0900

    背景色をスクロール位置によって変更

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 (function () {
 
-    document.addEventListener('DOMContentLoaded', function () {
-        main();
-    });
-      
+    document.addEventListener('DOMContentLoaded', main);
+
     async function main()
     {
         const titleElement = document.getElementById('title');
-        const githubElement = document.getElementById('github');
-        const zennElement = document.getElementById('zenn');
         const titleText = 'Portfolio';
         const blinkCusrsorInterval = 500;
 
+        // initialize
+        initBackgroundColorChanger();
+
+        // animation
         await playCursorWaitAnimation(titleElement, 2000, blinkCusrsorInterval);
         await playTypeWriterAnimation(titleElement, titleText);
         addBlinkUnderscore(titleElement, blinkCusrsorInterval);
@@ -21,6 +21,24 @@
     function sleep(msec){
         return new Promise(resolve => setTimeout(resolve, msec))
     };
+
+    function initBackgroundColorChanger() {
+        const bodyElement = document.querySelector('body');
+        const initialClassNameOfBody = bodyElement.className;
+        const windowHeight = window.innerHeight;
+
+        function setBackgroundColorByScrollPosition() {
+            const scrollPosition = window.scrollY;
+            if (scrollPosition < windowHeight) {
+                bodyElement.className = `${initialClassNameOfBody} color-section1`;
+            } else {
+                bodyElement.className = `${initialClassNameOfBody} color-section2`;
+            }
+        }
+
+        setBackgroundColorByScrollPosition();
+        window.addEventListener('scroll', setBackgroundColorByScrollPosition);
+    }
 
     function addBlinkUnderscore(parentElement, blinkInterval) {
         const spanElement = document.createElement('span');
